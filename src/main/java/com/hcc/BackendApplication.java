5a39a19e43af93424e0db861693258bb0347883d
gerzon <gerzon.udang@gmail.com>
2023-06-27 16:59:09 +0800

    for tomorrow

--- src/main/java/com/hcc/BackendApplication.java
+++ src/main/java/com/hcc/BackendApplication.java
@@ -9,6 +9,5 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
 //eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJteXVzZXJuYW1lIiwic2NvcGVzIjpbeyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJpYXQiOjE2ODc0MTM1MzUsImV4cCI6MTY4Nzg0NTUzNX0.GP5B5y5FLhVEJ9ZRvbkdWqWS0V6M4rFApnqDQ54uL8E
